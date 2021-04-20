7cd0f3115220a08c7245a3f4af0281d320aef345
Ramesh Fadatare <ramesh24fadatare@gmail.com>
2021-04-20 20:23:52 +0530

    Update JDBCUtils.java

--- src/net/javaguides/todoapp/utils/JDBCUtils.java
+++ src/net/javaguides/todoapp/utils/JDBCUtils.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 
 public class JDBCUtils {
 
-	private static String jdbcURL = "jdbc:mysql://localhost:3306/demo";
+	private static String jdbcURL = "jdbc:mysql://localhost:3306/todo_management";
 	private static String jdbcUsername = "root";
 	private static String jdbcPassword = "root";
 
